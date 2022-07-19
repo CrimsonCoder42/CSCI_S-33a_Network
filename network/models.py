3fd7692c76f278e3b98e41f488609dfd275ba69c
CrimsonCoder42 <nostro37@gmail.com>
2022-07-19 13:50:43 -0400

    models updated

--- network/models.py
+++ network/models.py
@@ -1,6 +1,38 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 class User(AbstractUser):
     pass
+
+
+# New Post
+class Post(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="post")
+    body = models.TextField(blank=True)
+    like_count = models.IntegerField(default=0)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def serialize(self):
+        return {
+            "id": self.user.id,
+            "body": self.body,
+            "like_count": self.like_count,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p"),
+        }
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_profile")
+    user_bio = models.TextField(blank=True)
+    following = models.ManyToManyField("User", related_name="following_users")
+    followers = models.ManyToManyField("User", related_name="followers_users")
+
+    def serialize(self, user):
+        return {
+            "id": self.id,
+            "username": self.user.username,
+            "following": self.followers.count(),
+            "followers": self.followers.count(),
+        }
+
--- network/views.py
+++ network/views.py
@@ -1,8 +1,12 @@
+import json
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import User
 
